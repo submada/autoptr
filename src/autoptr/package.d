79a1f4ba3b0fc86f0e13fb44ab37b24ca218cdde
vitamin <vitamin@vitamin>
2022-01-10 23:32:29 +0100

    doc

--- src/autoptr/package.d
+++ src/autoptr/package.d
@@ -1,3 +1,16 @@
+/**
+    Imports all modules.
+
+    `autoptr.shared_ptr` : `autoptr.shared_ptr.SharedPtr`
+
+    `autoptr.rc_ptr` : `autoptr.rc_ptr.RcPtr`
+
+    `autoptr.intrusive_ptr` : `autoptr.intrusive_ptr.IntrusivePtr`
+
+    `autoptr.unique_ptr` : `autoptr.unique_ptr.UniquePtr`
+
+    `autoptr.common`
+*/
 module autoptr;
 
 public{
