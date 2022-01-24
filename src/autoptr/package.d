0bcd9e159371062bc5a095fb6c9df9a9d6a72989
vitamin <vitamin@vitamin>
2022-01-24 17:52:33 +0100

    move to BTL

--- src/autoptr/package.d
+++ src/autoptr/package.d
@@ -1,22 +1,3 @@
-/**
-    Imports all modules.
-
-    `autoptr.shared_ptr` : `autoptr.shared_ptr.SharedPtr`
-
-    `autoptr.rc_ptr` : `autoptr.rc_ptr.RcPtr`
-
-    `autoptr.intrusive_ptr` : `autoptr.intrusive_ptr.IntrusivePtr`
-
-    `autoptr.unique_ptr` : `autoptr.unique_ptr.UniquePtr`
-
-    `autoptr.common`
-*/
 module autoptr;
 
-public{
-    import autoptr.common;
-    import autoptr.shared_ptr;
-    import autoptr.intrusive_ptr;
-    import autoptr.rc_ptr;
-    import autoptr.unique_ptr;
-}
+public import btl.autoptr;
