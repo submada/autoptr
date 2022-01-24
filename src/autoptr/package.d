eb60090416d8415f15b8c116f9251ec10f74e50c
vitamin <vitamin@vitamin>
2022-01-24 17:55:30 +0100

    move to BTL

--- src/autoptr/package.d
+++ src/autoptr/package.d
@@ -1,3 +1,9 @@
 module autoptr;
 
-public import btl.autoptr;
+public{
+    import autoptr.common;
+    import autoptr.shared_ptr;
+    import autoptr.intrusive_ptr;
+    import autoptr.rc_ptr;
+    import autoptr.unique_ptr;
+}
